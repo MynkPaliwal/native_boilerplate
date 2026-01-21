44f4670ad99f8fc490b05cc674e30033ce39e4f9
MayankP <paliwal.mayank2010@gmail.com>
2026-01-21 15:25:11 +0530

    Minor change

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 const LoginScreen: React.FC = () => {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Login Screen</Text>
+            <Text>Login Screen 1</Text>
         </View>
     );
 };
