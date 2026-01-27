628bc6cd9da9897f65700bc6563b81ae55a56542
Mayank Paliwal <paliwal.mayank2010@gmail.com>
2026-01-27 16:35:43 +0530

    Updated comments.

--- src/redux/Services/ApiService.ts
+++ src/redux/Services/ApiService.ts
@@ -3,12 +3,12 @@ import { callRestApi } from './callRestApi.ts';
 import { apiKeys } from './ApiMapping';
 import { User, DeleteUserRequest } from './Types';
 
-export const ApiService = createApi({
-  reducerPath: 'api',
+export const ApiService = createApi({ //This creates one API service Internally, RTK Query will: Create reducers, Create middleware, Create cache logic, Track request lifecycle, You usually create one API slice per backend (or major domain)
+  reducerPath: 'api', //Defines where in Redux state RTK Query will store its cache
   baseQuery: callRestApi,
-  tagTypes: ['Users', 'Posts'],
-  endpoints: builder => ({
-    getUsers: builder.query({
+  tagTypes: ['Users', 'Posts'], //tagTypes defines the types of cache labels that RTK Query can use to automatically refetch or invalidate data.
+  endpoints: builder => ({ //endpoints is where you declare all your API calls (queries & mutations).
+    getUsers: builder.query({ //Defines a read-only query endpoint | Used for GET requests
       query: () => {
         return apiKeys.GET_USERS;
       },
