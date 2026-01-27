d09c3c6f216d6d4937f475dcaee7a2b082dddd82
Mayank Paliwal <paliwal.mayank2010@gmail.com>
2026-01-27 14:51:39 +0530

    Integrated Async storage.

--- App.tsx
+++ App.tsx
@@ -9,18 +9,20 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
-
 import RootNavigator from './src/navigation/RootNavigator';
 import { store } from './src/redux/Store';
+import ErrorBoundary from './src/components/ErrorBoundary';
 
 const App = (): React.JSX.Element => {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <RootNavigator />
-        </NavigationContainer>
-      </SafeAreaProvider>
+      <ErrorBoundary>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <RootNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </ErrorBoundary>
     </Provider>
   );
 };
