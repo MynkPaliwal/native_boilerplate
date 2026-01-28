61909bd5ee1f975e601955c2a166aee929dea9c6
Mayank Paliwal <paliwal.mayank2010@gmail.com>
2026-01-28 18:11:31 +0530

    Added comments.

--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -5,10 +5,10 @@ import userReducer from './reducers/UserSlice';
 
 export const store = configureStore({
   reducer: {
-    [ApiService.reducerPath]: ApiService.reducer,
+    [ApiService.reducerPath]: ApiService.reducer,  //ApiService is created using RTK Query (createApi), reducerPath is usually 'api', This line registers RTK Query’s internal reducer into Redux
     users: userReducer,
   },
-  middleware: (getDefaultMiddleware) =>
+  middleware: (getDefaultMiddleware) =>  //Middleware sits between dispatching an action and the reducer.
     getDefaultMiddleware().concat(ApiService.middleware),
 });
 
